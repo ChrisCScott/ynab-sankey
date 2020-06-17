d6ac804fbd548ebc9225aafede842e681f79b7db
Christopher Scott <christopher@christopherscott.ca>
2020-06-16 21:50:53 -0700

    Adding Sankey diagram template.
    This is incomplete (e.g. need to add code for actually drawing the
    Sankey diagram!), but it's a start.

---
+++ src/components/Sankey.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="container">
+    <h5>Budget Flows</h5>
+    <!-- This div will get populated via Javascript: -->
+    <div class="sankey" id="sankey-chart">
+    </div>
+  </div>
+</template>
+
+<script>
+
+import {utils} from 'ynab';
+import {GoogleCharts} from 'google-charts';
+
+// Load Google's Sankey chart-drawing code:
+GoogleCharts.load(drawChart, {
+    'packages': ['sankey']
+});
+
+function drawChart() {
+    
+}
+
+export default {
+  props: ['transactions'],
+  methods: {
+    // Now we can make this method available to our template
+    // So we can format this milliunits in the correct currency format
+    convertMilliUnitsToCurrencyAmount: utils.convertMilliUnitsToCurrencyAmount
+  }
+}
+</script>
